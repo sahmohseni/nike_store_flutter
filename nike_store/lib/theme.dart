40e3148e2ffe6efe5426113f2b2cf50d542f4fbf
sahmohseni <mohseni.seyedamirhessam@gmail.com>
2023-03-12 17:34:42 +0330

    add packages and theme config

---
+++ nike_store/lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LightTheme {
+  static const Color scaffoldBackGroundColor =
+      Color.fromARGB(255, 237, 240, 243);
+  static const Color primaryColor = Color.fromARGB(255, 53, 92, 115);
+  static const Color itemBackGroundColor = Color.fromARGB(255, 184, 196, 207);
+  static const Color primaryTextColor = Colors.black;
+}
